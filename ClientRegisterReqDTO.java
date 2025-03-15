6a3f56543042c414aa0d51953c05880c1b8b066d
RockKanchana <rockkanchana2003@gmail.com>
2025-03-15 11:59:42 +0530

    Add files via upload
    business request completed

---
+++ ClientRegisterReqDTO.java
@@ -0,0 +1,26 @@
+package com.lmu.gasbygas.dto.request;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.lmu.gasbygas.entity.RoleEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientRegisterReqDTO {
+
+    private String name;
+    private String email;
+    private String password;
+    private String address;
+    private String contact;
+    private RoleEntity roleId;
+    private String nic;
+    private String registrationNumber;
+    private MultipartFile[] certification;
+    
+}
